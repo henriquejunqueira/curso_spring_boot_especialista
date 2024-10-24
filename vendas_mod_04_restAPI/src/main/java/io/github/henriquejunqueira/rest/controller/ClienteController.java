ff18b1e6755a61c03faf55d0275a33dc647a3af1
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-24 00:20:49 -0300

    Mapping Requests with Request Mapping

--- vendas_mod_04_restAPI/src/main/java/io/github/henriquejunqueira/rest/controller/ClienteController.java
+++ vendas_mod_04_restAPI/src/main/java/io/github/henriquejunqueira/rest/controller/ClienteController.java
@@ -1,18 +1,24 @@
 package io.github.henriquejunqueira.rest.controller;
 
+import io.github.henriquejunqueira.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+//    @RequestMapping(value = "/hello/{nome}",method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/hello/{nome}", "/api/hello"}, // posso passar uma lista de url para acessar o mesmo metodo
+            method = RequestMethod.POST,
+            consumes = {"application/json", "application/xml"}, // no corpo da requisição pode vir um json ou um xml
+            produces = {"application/json", "application/xml"} // forma de retornar os objetos como json ou xml
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
+//    public String helloCliente(@PathVariable("nome") String nomeCliente){
+//    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
+    public Cliente helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
         return String.format("Hello %s ", nomeCliente);
     }
 
