2692cf72fd6c890ed9f2786b1c426a02fa7b3e02
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-29 11:26:44 -0300

    Configuring Roles and Authorities

--- vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/config/SecurityConfig.java
+++ vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/config/SecurityConfig.java
@@ -40,33 +40,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("fulano") // passa um usuário em memória
                 .password(passwordEncoder().encode("123")) // senha do usuário em memória criptografada
                 //.authorities("MANTER USUARIO"); // define uma autoridade
-                .roles("USER"); // define o perfil de usuário (nível de acesso)
+                //.roles("USER"); // define o perfil de usuário (nível de acesso)
+                .roles("USER", "ADMIN");
     }
 
     // metodo que trabalha a autorização. Ele pega quem for autenticado e verifica o nível de acesso
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//            .csrf().disable() // permite a segurança entre uma app web e o backend, porém não terá necessidade
+//            .authorizeRequests() // define a autorização das requisições
+//            //.antMatchers("/api/clientes/**") // define quem pode acessar o que
+//            //.antMatchers("/api/clientes/**").authenticated() // define que precisa estar autenticado
+//            // .antMatchers("/api/clientes/**").hasRole("USER") // define que precisa ter role (perfil) como "USER"
+              // .antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN") // recebe mais de uma role
+//            // .antMatchers("/api/clientes/**").hasAuthority("MANTER USUARIO") // define que precisa ter autoridade
+//            // .antMatchers("/api/clientes/**").permitAll() // define que o acesso é permitido pra todos
+//            .antMatchers("/api/clientes/**").authenticated()
+//            .and() // volta pra raiz
+//            //.formLogin() // habilita o formulário de login do spring
+//            //.formLogin("/meu-login.html") // define o caminho de um formulário customizado sendo ele POST
+//            .formLogin();
+//            /**
+//            * o formuário customizado deve ter os seguintes atributos
+//            * <form method="post>
+//            *     <input type="text" name="username" />
+//            *     <input type="secret" name="password" />
+//            *     <button type="submit">Entrar</button>
+//            * </form>
+//            */
+
+        // Configurando as Roles e Authorities
         http
-            .csrf().disable() // permite a segurança entre uma app web e o backend, porém não terá necessidade
-            .authorizeRequests() // define a autorização das requisições
-            //.antMatchers("/api/clientes/**") // define quem pode acessar o que
-            //.antMatchers("/api/clientes/**").authenticated() // define que precisa estar autenticado
-            // .antMatchers("/api/clientes/**").hasRole("USER") // define que precisa ter role (perfil) como "USER"
-            // .antMatchers("/api/clientes/**").hasAuthority("MANTER USUARIO") // define que precisa ter autoridade
-            // .antMatchers("/api/clientes/**").permitAll() // define que o acesso é permitido pra todos
-            .antMatchers("/api/clientes/**").authenticated()
-//            .antMatchers("/api/clientes/**").permitAll()
-            .and() // volta pra raiz
-            //.formLogin() // habilita o formulário de login do spring
-            //.formLogin("/meu-login.html") // define o caminho de um formulário customizado sendo ele POST
-            .formLogin();
-            /**
-            * o formuário customizado deve ter os seguintes atributos
-            * <form method="post>
-            *     <input type="text" name="username" />
-            *     <input type="secret" name="password" />
-            *     <button type="submit">Entrar</button>
-            * </form>
-            */
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/clientes/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/pedidos/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/api/produtos/**").hasRole("ADMIN")
+                .and().formLogin();
     }
 }
