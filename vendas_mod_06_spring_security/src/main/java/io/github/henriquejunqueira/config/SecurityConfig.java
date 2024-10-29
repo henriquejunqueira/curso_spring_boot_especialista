b1b100f7aaed15d4d22ebf3a9ec24f329fec23bf
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-29 10:27:49 -0300

    Working with Password encoder

--- vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/config/SecurityConfig.java
+++ vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/config/SecurityConfig.java
@@ -1,12 +1,35 @@
 package io.github.henriquejunqueira.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    // essa interface server pra criptografar e descriptografar a senha de um usuário
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+//        PasswordEncoder passwordEncoder = new PasswordEncoder() {
+//            // nesse metodo é criado o tipo de criptografia
+//            @Override
+//            public String encode(CharSequence charSequence) {
+//                return charSequence + "321"; // aqui posso concatenar a senha do usuário com 321 por exemplo
+//            }
+//
+//            // o metodo matches recebe a senha do usuário (charSequence) e a senha criptografada (s)
+//            @Override
+//            public boolean matches(CharSequence charSequence, String s) {
+//                return (charSequence + "321").equals(s); // compara a senha do usuário com a senha criptografada
+//            }
+//        }
+
+        return new BCryptPasswordEncoder(); // sempre que o bcrypt é chamado ele gera uma hash diferente
+    }
 
     // metodo que trabalha a autenticação
     @Override
