ec1bd6c7c72515ec69e5f5880c860a637049f674
Henrique Junqueira <41648535+henriquejunqueira@users.noreply.github.com>
2024-10-30 11:24:47 -0300

    Implemented user registration using spring security

--- vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/service/impl/UsuarioServiceImpl.java
+++ vendas_mod_06_spring_security/src/main/java/io/github/henriquejunqueira/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,7 @@
 package io.github.henriquejunqueira.service.impl;
 
+import io.github.henriquejunqueira.domain.entity.Usuario;
+import io.github.henriquejunqueira.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,6 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.StringJoiner;
 
 @Service
 public class UsuarioServiceImpl implements UserDetailsService {
@@ -14,19 +19,40 @@ public class UsuarioServiceImpl implements UserDetailsService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
     // carrega os usuários do bd
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        if(!username.equals("cicrano")){
-            throw new UsernameNotFoundException("Usuário não encontrado na base de dados.");
-        }
+//        if(!username.equals("cicrano")){
+//            throw new UsernameNotFoundException("Usuário não encontrado na base de dados.");
+//        }
+//
+//        return User
+//                .builder()
+//                .username("cicrano")
+//                .password(passwordEncoder.encode("123"))
+//                .roles("USER", "ADMIN")
+//                .build();
+
+        Usuario usuario = usuarioRepository
+                .findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado na base de dados."));
+
+        String[] roles = usuario.isAdmin() ? new String[]{"ADMIN", "USER"} : new String[]{"USER"};
 
         return User
                 .builder()
-                .username("cicrano")
-                .password(passwordEncoder.encode("123"))
-                .roles("USER", "ADMIN")
+                .username(usuario.getLogin())
+                .password(usuario.getSenha())
+                .roles(roles)
                 .build();
     }
 }
